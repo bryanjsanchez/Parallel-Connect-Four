7357963249fd4e59212804f714a47be4b829a148
bryanjsanchez <bryanjsanchez@gmail.com>
2019-05-05 14:34:19 -0400

    Refactor code
    - Random starting player
    - Modify minimax terminal node conditions

--- connectfour.py
+++ connectfour.py
@@ -3,7 +3,6 @@ import numpy as np
 import random
 import math
 
-
 ROWS = 6
 COLUMNS = 7
 EMPTY = 0
@@ -15,18 +14,11 @@ MAX_SPACE_TO_WIN = 3 # Farthest space where a winning connection may start
 def create_board():
     return np.zeros((ROWS, COLUMNS), np.int8)
 
-#checks if column is full or not
+# Checks if column is full or not
 def is_valid_column(board, column):
     return board[0][column - 1] == EMPTY
 
-
-#def next_valid_row(board, column):
-#    for r in range(1,7):
-#        if board[r][column] == EMPTY:
-#            return r
-
-
-#returns list of columns that are still valid to play
+# Returns list of columns that are still not full
 def valid_locations(board):
     valid_locations = []
     for i in range(1,8):
@@ -34,21 +26,20 @@ def valid_locations(board):
            valid_locations.append(i)
     return valid_locations
 
-
 def place_piece(board, player, column):
     index = column - 1
     for row in reversed(range(ROWS)):
         if board[row][index] == EMPTY:
             board[row][index] = player
             return
-        
-#returns a successor board
+
+# Returns a successor board
 def clone_and_place_piece(board, player, column):
     new_board = board.copy()
     place_piece(new_board, player, column)
     return new_board
 
-#checks if the move made was a winning move
+# Checks if the player won the given board
 def detect_win(board, player):
     # Horizontal win
     for col in range(COLUMNS - MAX_SPACE_TO_WIN):
@@ -76,14 +67,12 @@ def detect_win(board, player):
                 return True
     return False
     
-
-#returns true if current board is a terminal board which happens when 
+# Returns true if current board is a terminal board which happens when 
 # either player wins or no more spaces on the board are free
 def is_terminal_board(board):
     return detect_win(board, HUMAN) or detect_win(board, AI) or \
         len(valid_locations(board)) == 0
         
-
 def score(board, player):
     score = 0
     # Give more weight to center columns
@@ -91,9 +80,9 @@ def score(board, player):
         for row in range(ROWS):
             if board[row][col] == player:
                 if col == 3:
-                    score += 6
+                    score += 3
                 else:
-                    score+=5
+                    score+= 2
     # Horizontal pieces
     for col in range(COLUMNS - MAX_SPACE_TO_WIN):
         for row in range(ROWS):
@@ -120,7 +109,6 @@ def score(board, player):
             score += evaluate_adjacents(adjacent_pieces, player)
     return score
 
-
 def evaluate_adjacents(adjacent_pieces, player):
     opponent = AI
     if player == AI:
@@ -136,58 +124,14 @@ def evaluate_adjacents(adjacent_pieces, player):
             empty_spaces += 1
         elif p == opponent:
             opponent_pieces += 1
-
     if player_pieces == 4:
-        score += 999
+        score += 99999
     elif player_pieces == 3 and empty_spaces == 1:
-        score += 10 
+        score += 100
     elif player_pieces == 2 and empty_spaces == 2:
-        score += 4
-    elif opponent_pieces == 3 and empty_spaces == 1:
-        score -= 8
-#    elif player_pieces == 1 and opponent_pieces == 3:
-#        score += 100
+        score += 10
     return score
 
-
-#manages ai behavior
-#def minimax(board, ply, maxi_player):
-#    valid_cols = valid_locations(board)
-#    is_terminal = is_terminal_board(board)
-#    if ply == 0 or is_terminal:
-#        if is_terminal:
-#            if detect_win(board, HUMAN):
-#                return (None,-1000000000)
-#            if detect_win(board, AI):
-#                return (None,1000000000)
-#            #No more valid moves
-#            if ply == 0:
-#                return (None,0)
-#        else:
-#            return (None,score(board, AI))
-#    #if max player
-#    if maxi_player:
-#        value = -math.inf
-#        col = random.choice(valid_cols)
-#        for c in valid_cols:
-#            next_board = clone_and_place_piece(board, AI, c)
-#            new_score = minimax(next_board, ply - 1, False)[1]
-#            if new_score > value:
-#                value = new_score
-#                col = c
-#        return col, value
-#        #if min player
-#    else:
-#        value = math.inf
-#        col = random.choice(valid_cols)
-#        for c in valid_cols:
-#            next_board = clone_and_place_piece(board, HUMAN, c)
-#            new_score = minimax(next_board, ply - 1, True)[1]
-#            if new_score < value:
-#                value = new_score
-#                col = c
-#        return col, value
-    
 def minimax(board, ply, alpha, beta, maxi_player):
     valid_cols = valid_locations(board)
     is_terminal = is_terminal_board(board)
@@ -195,27 +139,28 @@ def minimax(board, ply, alpha, beta, maxi_player):
         if is_terminal:
             if detect_win(board, HUMAN):
                 return (None,-1000000000)
-            if detect_win(board, AI):
+            elif detect_win(board, AI):
                 return (None,1000000000)
-            #No more valid moves
-            if ply == 0:
+            else: # There is no winner
                 return (None,0)
-        else:
+        else: # Ply == 0
             return (None,score(board, AI))
-    #if max player
+    # If max player
     if maxi_player:
         value = -math.inf
+        # If every choice has an equal score, choose randomly
         col = random.choice(valid_cols)
+        # Expand current node/board
         for c in valid_cols:
             next_board = clone_and_place_piece(board, AI, c)
             new_score = minimax(next_board, ply - 1, alpha, beta, False)[1]
             if new_score > value:
                 value = new_score
                 col = c
-            #alpha pruning
+            # Alpha pruning
             if value > alpha:
                 alpha = new_score
-            #if beta is less than or equal to alpha, there will be no need to
+            # If beta is less than or equal to alpha, there will be no need to
             # check other branches because there will not be a better move
             if beta <= alpha:
                 break
@@ -235,7 +180,6 @@ def minimax(board, ply, alpha, beta, maxi_player):
             if beta <= alpha:
                 break
         return col, value
-        
 
 def draw_game(board, turn, game_over=False, AI_move=0, running_time=0):
     highlight_index = AI_move - 1
@@ -281,16 +225,15 @@ def draw_game(board, turn, game_over=False, AI_move=0, running_time=0):
             print("             Minimax running time: %.4f seconds" % running_time)
             print("Your move: ")
         else:
-            print("Waiting for computer...")
-    
+            print("\nWaiting for computer...")
 
-# Starter sample
+# Start of game loop:
 board = create_board()
-turn = HUMAN
-draw_game(board, turn)
+turn = random.choice([HUMAN, AI])
 is_game_won = False
 AI_move = -1
 running_time = 0
+draw_game(board, turn)
 while not is_game_won:
     if turn == HUMAN:
         # Take user input
@@ -299,7 +242,6 @@ while not is_game_won:
             pressed_key = int(pressed_key)
         except ValueError:
             pass      
-        
         # If typed 1 to 7
         if pressed_key in range(1,8) and is_valid_column(board, pressed_key):
             place_piece(board, HUMAN, pressed_key)
@@ -316,33 +258,18 @@ while not is_game_won:
         elif pressed_key == "q":
             print( "\033c") # Clear screen
             print("\nThank you for playing!")
-            break
-            #exit()
+            exit()
         # Invalid input
         else:
            print("\nInvalid input, try again...")
+           time.sleep(1)
            draw_game(board, turn, AI_move = AI_move, running_time=running_time)
 
     elif turn == AI:
-        # Placeholder AI move. It will change:
         initial_time = time.time()
-#        best_move = -1
-#        best_score = 0
-        #for alpha-beta pruning we initialize minimax with worse value for
-        #alpha, -inf, and worse value for beta, inf.
-        col, minimax_value = minimax(board, 4, -math.inf, math.inf, True)
-        AI_move = col
-#        for col in range(1,8):
-#            current_score = score(clone_and_place_piece(board, AI, col), AI)
-#            if current_score > best_score:
-#                best_move = col
-#                best_score = current_score
-#        if best_move in range(1,8):
-#            AI_move = best_move
-#        else:
-#            AI_move = random.randrange(1,8)
-#            while not is_valid_column(board, AI_move):
-#                AI_move = random.randrange(1,8)
+        # For alpha-beta pruning we initialize minimax with worse value for
+        # alpha, -inf, and worse value for beta, inf.
+        AI_move, minimax_value = minimax(board, 5, -math.inf, math.inf, True)
         place_piece(board, AI, AI_move)
         is_game_won = detect_win(board, AI)
         running_time = time.time() - initial_time
@@ -354,6 +281,6 @@ while not is_game_won:
             turn = HUMAN
             draw_game(board, turn, AI_move=AI_move, running_time=running_time)
             continue
-print("                    Thank you for playing!")
-print("              Minimax running time: %.4f seconds" % running_time)
-#exit()
+if is_game_won:
+    print("                    Thank you for playing!")
+    print("              Minimax running time: %.4f seconds" % running_time)
